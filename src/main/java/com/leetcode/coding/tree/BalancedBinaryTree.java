1f0a13eb54f2a6dcdef32dec93a20c311e49e05f
pgpgpgg <975022570yp@gmail.com>
2020-07-23 00:20:47 +0800

    add O(n) solution

--- src/main/java/com/leetcode/coding/tree/BalancedBinaryTree.java
+++ src/main/java/com/leetcode/coding/tree/BalancedBinaryTree.java
@@ -2,6 +2,13 @@ package com.leetcode.coding.tree;
 
 public class BalancedBinaryTree {
 
+    private boolean balanced;
+
+    /**
+     * O(logN)
+     * @param root
+     * @return
+     */
     public static boolean isBalanced(TreeNode root) {
         if (root == null) {
             return  true;
@@ -16,7 +23,25 @@ public class BalancedBinaryTree {
         if (root == null) {
             return 0;
         }
-
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+
+    /************************************** O(logN)*****************************************/
+    private int height(TreeNode root, boolean balanced) {
+        if (root == null || !balanced) return -1;
+        int l = height(root.left, balanced);
+        int r = height(root.right, balanced);
+        if (Math.abs(l - r) > 1) {
+            balanced = false;
+            return -1;
+        }
+        return Math.max(l, r) + 1;
+    }
+
+    public boolean isBalancedEffective(TreeNode root) {
+        boolean balanced = true;
+        height(root, balanced);
+        return balanced;
     }
 }
