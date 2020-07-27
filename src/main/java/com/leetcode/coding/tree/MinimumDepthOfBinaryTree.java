518c65ac61c5143b735ea699c73a9b5dcd06e3a8
pgpgpgg <975022570yp@gmail.com>
2020-07-27 08:31:07 +0800

    add comment

--- src/main/java/com/leetcode/coding/tree/MinimumDepthOfBinaryTree.java
+++ src/main/java/com/leetcode/coding/tree/MinimumDepthOfBinaryTree.java
@@ -3,6 +3,7 @@ package com.leetcode.coding.tree;
 public class MinimumDepthOfBinaryTree {
 
     public static int minDepth(TreeNode root) {
+        //注意[1,2] 这种测试用例
 //        if (root == null) {
 //            return 0;
 //        }
@@ -15,6 +16,11 @@ public class MinimumDepthOfBinaryTree {
         if (root.left == null && root.right == null) return 1;
         int l = root.left != null ? minDepth(root.left) : Integer.MAX_VALUE;
         int r = root.right != null ? minDepth(root.right) : Integer.MAX_VALUE;
+
         return Math.min(l, r) + 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Math.min(5,5));
     }
 }
